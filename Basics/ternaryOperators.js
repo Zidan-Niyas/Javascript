7d9ddb91dee1b49b89cd73f371a968573bf1ee11
Zidan Niyas <87656623+Zidan-Niyas@users.noreply.github.com>
2024-02-19 14:54:55 +0530

    Create ternaryOperators.js

---
+++ Basics/ternaryOperators.js
@@ -0,0 +1,5 @@
+// Ternary Operators (alternative to if-else statements ---> important for REACT)
+let myName = age > 10 ? "Zidan" : "Jack";  
+const Component = () => {
+    return age > 10 ? <div>Zidan</div> : <div>Jack</div>;
+}
