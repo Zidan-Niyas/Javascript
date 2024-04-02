89c6b8296a53813dcab74232492315d042567407
Zidan Niyas <87656623+Zidan-Niyas@users.noreply.github.com>
2024-04-03 01:39:53 +0530

    Update promiseAPIs.js

--- Basics/promiseAPIs.js
+++ Basics/promiseAPIs.js
@@ -6,6 +6,18 @@ Promise APIs :
     4) Promise.any()
 */
 
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("P1 success"), 3000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(() => reject("P2 fail"), 1000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("P3 success"), 2000);
+});
+
 Promise.all([p1, p2, p3]);  // all or nothing
 // p1,p2,p3 are promises
 // p1 --> 3s,  p2 --> 1s,  p3 --> 2s
